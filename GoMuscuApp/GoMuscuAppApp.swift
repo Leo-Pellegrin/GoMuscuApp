ac6b72b18ee3d7fe190d31cdc61fd1fae3d49cd1
Leo-Pellegrin <leo.pellegrin@etud.univ-jfc.fr>
2024-01-15 09:03:57 +0100

    Initial Commit

---
+++ GoMuscuApp/GoMuscuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoMuscuAppApp.swift
+//  GoMuscuApp
+//
+//  Created by Léo Pellegrin on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoMuscuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
